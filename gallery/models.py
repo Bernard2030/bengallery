bb1ff3f655964f7b547a993a9a6ce29c8e47127c
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-07 15:00:35 +0300

    add model class

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    Image= models.ImageField(upload_to='images/')
+    Name = models.CharField(max_length =30)
+    Description = models.CharField(max_length =60)
+    Location = models.ForeignKey(Location,on_delete=models.CASCADE)
+    category = models.ForeignKey(category,on_delete=models.CASCADE,default=None)
