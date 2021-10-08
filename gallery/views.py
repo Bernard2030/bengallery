a2473e800817a5a4d2ce6bd7457ced713fdb8e01
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-08 12:11:12 +0300

    add search result in views

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Image, Location, Category
 
 # Create your views here.
 def welcome(request):
     return render(request,'welcome.html')
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_image = Image.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-gallery/search.html',{"message":message,"images": searched_image})
+
+    else:
+        message = "You haven't searched for any image category"
+        return render(request, 'all-gallery/search.html',{"message":message})    
